e8654f20252fc9a4778d9012d97a724e5d675095
reidizhupa <reidizhupa56@gmail.com>
2026-03-31 04:21:31 +0900

    hero, how

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -20,6 +20,7 @@ import { AppLocale } from "@/i18n/config";
 import PricingSection from "../components/homepage/Pricing";
 import ProgramsTabs from "@/app/components/homepage/ProgramsTabs";
 import Navbar from "@/app/components/Navbar";
+import ContinuousLearningCycle from "../components/homepage/HowItWorks";
 
 // app/[locale]/page.tsx
 export async function generateMetadata(props: { params: Promise<{ locale: AppLocale }> }, parent: ResolvingMetadata) {
@@ -168,26 +169,22 @@ export default function HomePage() {
 	};
 	return (
 		<>
-			
 			<HeroSection />
+			<LogoSection />
 			<div id="why-jbs" className="scroll-mt-24">
 				<JGlobalBusinessSchool />
 			</div>
-			<JGlobalAchievements />
-			<LogoSection />
-			<FreeTrial />
+			<ContinuousLearningCycle />
 			<div id="programs" className="bg-[#dbe9ff] pt-20 scroll-mt-16">
 				<div className="bg-[url('/img/bg_continuation.png')] bg-cover bg-center bg-no-repeat">
 					<h1 className="text-2xl md:text-5xl text-center flex justify-center lg:text-5xl font-extrabold tracking-tight text-gray-600 mb-10">
 						<span className="bg-clip-text  relative z-10"> {tHome("programsSectionSubtitle")}</span>
 					</h1>
-					<ProgramsTabs 
-						levelsData={[globalPrograms, globalTeamwork, globalLeadership]} 
-					/>
+					<ProgramsTabs levelsData={[globalPrograms, globalTeamwork, globalLeadership]} />
 				</div>
 			</div>{" "}
 			<div id="pricing" className="scroll-mt-16">
-			<PricingSection />
+				<PricingSection />
 			</div>
 			<JBSWorkshopsBenefits />
 			<JBSWorkshopsStructure />
@@ -197,28 +194,6 @@ export default function HomePage() {
 			</div>
 			<FeturedIn />
 			<Footer />
-			<Link
-				href="/free-coaching"
-				className="
-    fixed bottom-6 right-0
-    bg-linear-to-r from-[#f58766]/80 to-[#fd634b]/80
-    backdrop-blur-md
-  
-    shadow-lg shadow-blue-500/30
-    pl-5 pr-7 py-3
-    text-white font-semibold tracking-tight
-    rounded-l-2xl
-    flex items-center gap-2
-    transition-all duration-300
-    hover:shadow-xl hover:shadow-blue-400/50
-    hover:from-[#ffa78c] hover:to-[#ff885c]
-    z-50
-  "
-			>
-				<span className="drop-shadow-md">{tHome("freeCoaching")}</span>
-
-				{/* Curved blue edge */}
-			</Link>
 		</>
 	);
 }
